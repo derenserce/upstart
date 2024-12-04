f6a6216c412a270e5007829ac9fd3ab2c06fc0fa
Rekouwn <sercederen@gmail.com>
2024-12-04 13:02:55 +0100

    fix2

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -14,7 +14,7 @@ import Link from "next/link";
 
 export const Navbar = () => {
   return (
-    <nav className="pb-safe-bottom mb-2 fixed bottom-0 left-0 right-0 z-50 flex h-14 w-full items-center justify-around border-t border-white bg-background shadow-[0_-2px_4px_rgba(0,0,0,0.1)] md:h-16">
+    <nav className="pb-safe-bottom mb-4 fixed bottom-0 left-0 right-0 z-50 flex h-14 w-full items-center justify-around border-t border-white bg-background shadow-[0_-2px_4px_rgba(0,0,0,0.1)] md:h-16">
       <Link
         href="/"
         className="flex flex-col items-center justify-center gap-1 text-sm font-medium text-muted-foreground hover:text-primary focus:text-primary"
