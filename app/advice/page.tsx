cdeaee399c7b021ddb13764bb6610a8998e12e79
Rekouwn <sercederen@gmail.com>
2024-12-04 12:57:06 +0100

    fix: light mode text

--- app/advice/page.tsx
+++ app/advice/page.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import { ModeToggler } from "@/components/mode-toggle";
+import React from "react";
 
 export default function Advice() {
   return (
-    <div className='px-6 pt-6'>Vraag advies aan ons adviseur +31 6 15935435</div>
-  )
+    <div className="px-6 pt-6">
+      Vraag advies aan ons adviseur +31 6 15935435
+      <ModeToggler />
+    </div>
+  );
 }
